d1bd84e086b806dbca65152d91915cac157c722e
Sergey P <pavlovdog.moscow@gmail.com>
2021-04-08 16:34:32 +0200

    Add deploy multiple accounts per one external call

--- contracts/AccountFactory.sol
+++ contracts/AccountFactory.sol
@@ -9,13 +9,10 @@ contract AccountFactory {
     uint256 static public owner;
     TvmCell static public account_code;
 
-    function deploy(
+    function deploy_(
         uint256 account_owner,
         uint128 account_value
-    ) external returns(address account) {
-        require(msg.pubkey() == owner, 101);
-        tvm.accept();
-
+    ) internal returns(address account) {
         account = new Account{
             value: account_value,
             code: account_code,
@@ -24,5 +21,19 @@ contract AccountFactory {
                 owner: account_owner
             }
         }();
+    }
+
+    function deploy(
+        uint256[] account_owners,
+        uint128[] account_values
+    ) external returns(address[] accounts) {
+        require(msg.pubkey() == owner, 101);
+        require(account_owners.length == account_values.length, 102);
+        tvm.accept();
+
+        for (uint i=0; i<account_owners.length; i++) {
+            address account = deploy_(account_owners[i], account_values[i]);
+            accounts.push(account);
+        }
     }
 }
